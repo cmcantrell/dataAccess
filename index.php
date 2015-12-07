1180fd42c99655a7b5c27b6ecc95bb60b7718060
Clinton Cantrell <clinton@Clintons-iMac.sandbox>
2015-12-07 08:55:12 -0700

    changes

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
-		$params = array(
-			//'SQL' => "SELECT * FROM sys_users",
-			'table' => array('sys_users'),
-			//'index' => array( '<>id', 'username', 'password', 'permission_id', 'user_role' ),
-			//'condition' => array('id'=>1, 'username'=>"cmcantrell", 'permission'=>99),
-			//'condition' => "where id=1 and username='cmcantrell'",
-			//'order' => array('id'=>'DESC', 'username'=>'ASC'),
-			//'order' => "order by id DESC",
-			//'limit' => 5,
-			//'offset' => 1,
-			'fetchAll' => true,
-		);
+		
 		
 		$db_access = new Client_MySQL();
 		
@@ -18,9 +7,4 @@
 		
 		//$result = $db_access->db_fetch( $params );
 		
-		$result = MySQL_select('sys_users', array('username','password'));
 		
-		echo "<h2>Output</h2>";
-		echo "<pre>";
-		print_r($result);
-		echo "</pre>";
